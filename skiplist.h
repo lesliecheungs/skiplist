4e5acb1353e348b31d5b85d2cf92573a77aca285
Yefimov <771523300@qq.com>
2023-03-04 19:07:05 +0800

    modified:   skiplist.h

--- skiplist.h
+++ skiplist.h
@@ -46,4 +46,56 @@ Node<K,V>::Node(const K k, const V v, int level) {
 
     memset(this->forward, 0, sizeof(Node<K,V>*) * (level+1));
 }
+
+template<typename K, typename V>
+Node<K, V>::~Node() {
+    delete []forward;
+}
+
+template<typename K, typename V>
+K Node<K, V>::get_key() const {
+    return key;
+}
+
+template<typename K, typename V>
+V Node<K, V>::get_value() const {
+    return value;
+}
+
+template<typename K, typename V>
+void Node<K, V>::set_value(V value) {
+    this->value = value;
+}
+
+template<typename K, typename V>
+class SkipList {
+public:
+    SkipList(int);
+    ~SkipList();
+
+    int get_random_level();
+    Node<K, V>* create_node(K, V, int);
+    int insert_element(K, V);
+    void display_list();
+    bool search_element(K);
+    void delete_element(K);
+    void dump_file();
+    void load_file();
+    int size();
+    
+private:
+    void get_key_value_from_string(const std::string& str, std::string* key, std::string* value);
+    bool is_valid_string(const std::string& str);
+private:
+    int _max_level;
+
+    int _skip_list_level;
+
+    Node<K, V> *_header;
+
+    std::ofstream _file_writer;
+    std::ifstream _file_reader;
+
+    int _element_count;
+};
 #endif
