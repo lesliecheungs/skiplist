9dd89d6a8a27eea6f6eeb1d33fb58770b84b1d10
Yefimov <771523300@qq.com>
2023-03-05 15:28:08 +0800

    modified:   main.cpp 	modified:   skiplist.h

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 #define FILE_PATH "./store/dumpFile"
 
 int main() {
+    SkipList<int, std::string> skipList(6);
+    skipList.insert_element(1, "a"); 
+	skipList.insert_element(3, "b"); 
+	skipList.insert_element(7, "c"); 
+	skipList.insert_element(8, "d"); 
+	skipList.insert_element(9, "e"); 
+	skipList.insert_element(19, "f"); 
+	skipList.insert_element(19, "g"); 
 
 
+    skipList.display_list();
     return 0;
 }
--- skiplist.h
+++ skiplist.h
@@ -7,6 +7,7 @@
 #include <cstring>
 #include <mutex>
 #include <fstream>
+#include <algorithm>
 
 #define STORE_FILE "store/dumpFile"
 
@@ -91,11 +92,143 @@ private:
 
     int _skip_list_level;
 
-    Node<K, V> *_header;
+    Node<K, V>* _header;
 
     std::ofstream _file_writer;
     std::ifstream _file_reader;
 
     int _element_count;
 };
+
+// 创建新节点
+template<typename K, typename V>
+Node<K, V>* SkipList<K, V>::create_node(const K k, const V v, int level) {
+    Node<K, V>* n = new Node<K, V>(k, v, level);
+
+    return n;
+}
+
+
+
+
+
+// 插入一个节点到跳表
+// 返回0代表插入成功， 1代表失败
+/* 
+                           +------------+
+                           |  insert 50 |
+                           +------------+
+level 4     +-->1+                                                      100
+                 |
+                 |                      insert +----+
+level 3         1+-------->10+---------------> | 50 |          70       100
+                                               |    |
+                                               |    |
+level 2         1          10         30       | 50 |          70       100
+                                               |    |
+                                               |    |
+level 1         1    4     10         30       | 50 |          70       100
+                                               |    |
+                                               |    |
+level 0         1    4   9 10         30   40  | 50 |  60      70       100
+                                               +----+
+*/
+
+template<typename K, typename V>
+int SkipList<K, V>::insert_element(const K key, const V value) {
+    mtx.lock();
+    Node<K, V>* current = this->_header;
+
+    Node<K, V>* update[_max_level+1];
+    memset(update, 0, sizeof(Node<K, V>*) * (_max_level+1));
+
+    for(int i = _skip_list_level; i>=0; i--) {
+        while(current->forward[i] != NULL && current->forward[i]->get_key() < key) {
+            current = current->forward[i];
+        }
+        update[i] = current;
+    }
+
+    current = current->forward[0];
+
+    if(current != NULL && current->get_key() == key) {
+        std::cout << "key: " << key << ", exists" << std::endl;
+        mtx.unlock();
+        return 1;
+    }
+
+    if(current == NULL || current->get_key() != key) {
+        int random_level = get_random_level();
+
+        if(random_level > _skip_list_level) {
+            for(int i = _skip_list_level+1; i <= random_level; i++) {
+                update[i] = _header;
+            }
+            _skip_list_level = random_level;
+        }
+
+        Node<K, V>* inserted_node = create_node(key, value, random_level);
+
+        for(int i=0; i<=random_level; i++) {
+            inserted_node->forward[i] = update[i]->forward[i];
+            update[i]->forward[i] = inserted_node;
+        }
+        std::cout << "Successfully inserted key:" << key << ", value:" << value << std::endl;
+        _element_count ++;
+    }
+    mtx.unlock();
+    return 0;
+}
+
+
+template<typename K, typename V>
+void SkipList<K, V>::display_list() {
+    std::cout << "\n*****Skip List*****"<<"\n"; 
+    for(int i=0; i <= _skip_list_level; i++) {
+        Node<K, V>* node = this->_header->forward[i];
+        std::cout << "Level " << i << ": ";
+        while(node != NULL) {
+            std::cout << node->get_key() << "(" << node->get_value() << ")->";
+            node = node->forward[i];
+        }
+        std::cout << std::endl;
+    }
+}
+
+// 构造函数
+template<typename K, typename V>
+SkipList<K, V>::SkipList(int max_level) {
+    this->_max_level = max_level;
+    this->_skip_list_level = 0;
+    this->_element_count = 0;
+
+    K k;
+    V v;
+    this->_header = new Node<K, V>(k, v, _max_level);
+}
+
+template<typename K, typename V>
+SkipList<K, V>::~SkipList() {
+    if(_file_writer.is_open()) {
+        _file_writer.close();
+    }
+    if(_file_reader.is_open()) {
+        _file_reader.close();
+    }
+    delete _header;
+}
+
+template<typename K, typename V>
+int SkipList<K, V>::get_random_level() {
+    // k最小得为1层
+    int k = 1;
+    // rand()随机生成一个整数，让k以百分之五十的概率递增
+    while(rand() % 2) {
+        k++;
+    }
+    // k不可超过设置的阈值
+    k = std::min(k, _max_level);
+
+    return k;
+}
 #endif
